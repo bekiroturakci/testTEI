33116812fdfc8abbc140e52b0e4080d0c6a62bbc
bekiroturakci <bekiroturakci@hotmail.com>
2023-09-14 16:20:04 +0300

    ilk proje

---
+++ ConsoleAppTEI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppTEI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teşekkürler TEI");
+        }
+    }
+}
